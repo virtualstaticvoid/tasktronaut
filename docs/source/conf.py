aec0cb006d069042846b10501c8a050a4a59670b
Chris Stefano <virtualstaticvoid@gmail.com>
2025-11-12 16:38:07 +0000

    fix member name resolution in API documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ extensions = [
 
 templates_path = ["_templates"]
 exclude_patterns = []
-
+add_module_names = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
